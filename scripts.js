11745e1bab9e1b3a727173c37632a50f7f33b8c0
Shemar Dacosta <dshemar6@gmail.com>
2019-02-28 09:50:19 -0500

    stuff

---
+++ scripts.js
@@ -0,0 +1,4 @@
+(function() {
+  document.getElementById("home-top").style.backgroundImage = "url('https://i.imgur.com/aFoneLb.jpg')"
+  document.getElementById("home-top").style.backgroundSize = "cover"
+})();
